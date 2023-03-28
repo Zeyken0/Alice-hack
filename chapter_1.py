b83d81f88e3b1f5e19372d668dbe467fbbd29285
Zeyken_0 <56526742+Zeyken0@users.noreply.github.com>
2023-03-28 22:55:12 +0700

    Update chapter_1.py

--- chapter_1.py
+++ chapter_1.py
@@ -84,8 +84,34 @@ def chap(req_save, command, intent, user_id):
     text = alice_dict['chap']['text']
     tts = alice_dict['chap']['tts']
     new_save = {'accept': 'chap', 'reject': ''}
-    return confirm_reject_handler(req_save, command, intent, text_commands=COMMANDS, text=text, tts=tts,
-                                  new_save=new_save)
+    card = 
+    {
+        "type": "ItemsList",
+        "header": {
+            "text": "Заголовок списка изображений",
+        },
+        "items": [
+            {
+                "image_id": "1030494/628705743a5ab80c90ea",
+                "title": "Заголовок для изображения.",
+                "description": "Описание изображения.",
+                "button": {
+                    "text": "Надпись на кнопке",
+                    "url": "https://example.com/",
+                    "payload": {}
+                }
+            }
+        ],
+        "footer": {
+            "text": "Текст блока под изображением.",
+            "button": {
+                "text": "Надпись на кнопке",
+                "url": "https://example.com/",
+                "payload": {}
+            }
+        }
+    return confirm_reject_handler_with_card(req_save, command, intent, text_commands=COMMANDS, text=text, tts=tts,
+                                  new_save=new_save, card= card)
 
 
 def chap_1(req_save, command, intent, user_id):
